dc5f18cc10f18fa609bd9b976e3aa6c2d10993f9
Miel Vander Sande <miel.vandersande@meemoo.be>
2022-04-22 15:20:35 +0200

    add files

---
+++ queries.py
@@ -0,0 +1,86 @@
+GET_DOC_MD = """
+PREFIX dct: <http://purl.org/dc/terms/>
+
+SELECT DISTINCT ?title ?created ?modified ?description
+WHERE { 
+    ?doc dct:title ?title.
+    FILTER(lang(?title) = ?lang)
+    OPTIONAL { ?doc dct:created ?created. }
+    OPTIONAL { ?doc dct:modified ?modified. }
+    OPTIONAL { 
+        ?doc dct:description ?description. 
+        FILTER(lang(?description) = ?lang)
+    }
+}
+"""
+
+
+GET_AUTHORS = """
+PREFIX dct: <http://purl.org/dc/terms/>
+PREFIX schema: <https://schema.org/>
+
+SELECT DISTINCT ?label ?email
+WHERE {
+    ?doc dct:author ?a.
+
+    ?a schema:name ?label.
+    OPTIONAL { ?a schema:email ?email. } 
+}
+"""
+
+GET_CLASSES = """
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+SELECT DISTINCT ?iri ?label ?description
+WHERE {
+    ?iri a rdfs:Class;
+        rdfs:label ?label.
+    FILTER(lang(?label) = ?lang)
+    OPTIONAL { 
+        ?iri rdfs:comment ?description. 
+        FILTER(lang(?description) = ?lang)
+    }
+}
+"""
+
+GET_SUBCLASSES = """
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+SELECT DISTINCT ?iri ?label ?description
+WHERE { 
+    ?iri   rdfs:subClassOf ?parent . 
+
+    ?iri a rdfs:Class;
+        rdfs:label ?label;
+        rdfs:comment ?description.
+}
+"""
+
+GET_SUPERCLASSES = """
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+SELECT DISTINCT ?iri ?label ?description
+WHERE { 
+    ?child   rdfs:subClassOf ?iri . 
+
+    ?iri a rdfs:Class;
+        rdfs:label ?label;
+        rdfs:comment ?description.
+}
+"""
+
+GET_PROPERTIES = """
+PREFIX sh: <http://www.w3.org/ns/shacl#>
+SELECT DISTINCT ?iri ?label ?description ?min ?max ?class ?datatype ?datatype_label
+WHERE {
+    ?class sh:property ?property.
+
+    ?property sh:path ?iri;
+            sh:name ?label;
+            sh:description ?description.
+
+    OPTIONAL {?property sh:minCount ?min}
+    OPTIONAL {?property sh:maxCount ?max}
+    OPTIONAL {?property sh:class ?class}
+    OPTIONAL {?property sh:datatype ?datatype}
+
+    OPTIONAL {?datatype rdfs:label ?datatype_label}
+}
+"""
---
+++ shacl2md.py
@@ -0,0 +1,104 @@
+import argparse
+
+from jinja2 import Environment, PackageLoader, select_autoescape
+from rdflib.graph import Graph
+from rdflib.namespace import DCTERMS, FOAF, OWL, RDF, RDFS, SKOS, Namespace
+from rdflib.term import BNode, Literal, URIRef
+
+from queries import (GET_AUTHORS, GET_CLASSES, GET_DOC_MD, GET_PROPERTIES,
+                     GET_SUBCLASSES, GET_SUPERCLASSES)
+
+SHACL = Namespace("http://www.w3.org/ns/shacl#")
+
+
+def get_doc(g):
+    for row in g.query(GET_DOC_MD, initBindings={"lang": Literal("nl")}):
+        row.authors = list(get_authors(g))
+        return row
+
+
+def get_classes(g):
+    classes = []
+    for row in g.query(GET_CLASSES, initBindings={"lang": Literal("nl")}):
+        classes.append(
+            {
+                "iri": row.iri,
+                "shortname": row.iri.n3(g.namespace_manager),
+                "label": row.label,
+                "description": row.description,
+                "parent": list(
+                    g.query(GET_SUPERCLASSES, initBindings={"child": row.iri})
+                ),
+                "child": list(
+                    g.query(GET_SUBCLASSES, initBindings={"parent": row.iri})
+                ),
+                "properties": list(get_properties(g, row.iri)),
+            }
+        )
+
+    return classes
+
+
+def get_properties(g, c=None):
+    properties = []
+    if c is not None:
+        qres = g.query(GET_PROPERTIES, initBindings={"class": c})
+    else:
+        qres = g.query(GET_PROPERTIES)
+    for row in qres:
+        properties.append(
+            {
+                "iri": row.iri,
+                "shortname": row.iri.n3(g.namespace_manager),
+                "label": row.label,
+                "description": row.description,
+                "datatype": {
+                    "label": row.datatype_label,
+                    "iri": row.datatype,
+                    "shortname": row.datatype.n3(g.namespace_manager),
+                },
+            }
+        )
+    return properties
+
+
+def get_authors(g):
+    return g.query(GET_AUTHORS)
+
+
+def main(args):
+
+    g = Graph()
+    for file in args.files:
+        g.parse(file)
+
+    env = Environment(
+        loader=PackageLoader("shacl2md"),
+        autoescape=select_autoescape(),
+        trim_blocks=True,
+    )
+
+    template = env.get_template("template.md.jinja")
+
+    print(
+        template.render(
+            doc=get_doc(g),
+            namespaces=g.namespace_manager.namespaces(),
+            properties=get_properties(g),
+            classes=get_classes(g),
+            authors=get_authors(g),
+        )
+    )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "files",
+        metavar="inputFile",
+        nargs="+",
+        help="SHACL OR RDFS files to construct Markdown documentation of.",
+    )
+    args = parser.parse_args()
+    # print(args.accumulate(args.files))
+    main(args)
