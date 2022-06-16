99443d43a46fa995acc97efc4d75a6f4a69e1af2
Miel Vander Sande <miel.vandersande@meemoo.be>
2022-06-16 16:59:54 +0200

    Format code

--- shacl2md.py
+++ shacl2md.py
@@ -6,7 +6,14 @@ from rdflib.namespace import DCTERMS, FOAF, OWL, RDF, RDFS, SKOS, Namespace
 from rdflib.term import BNode, Literal, URIRef
 from util import to_local_name, to_label, to_shortname
 
-from queries import (GET_AUTHORS, GET_CLASSES, GET_DOC_MD, GET_PROPERTIES, GET_SUBCLASSES, GET_SUPERCLASSES)
+from queries import (
+    GET_AUTHORS,
+    GET_CLASSES,
+    GET_DOC_MD,
+    GET_PROPERTIES,
+    GET_SUBCLASSES,
+    GET_SUPERCLASSES,
+)
 
 SHACL = Namespace("http://www.w3.org/ns/shacl#")
 
@@ -16,43 +23,52 @@ def get_doc(g, lang):
         row.authors = list(get_authors(g))
         return row
 
+
 def extract(g, row):
     return {
         "iri": row.iri,
         "shortname": to_shortname(g, row.iri),
         "label": to_label(g, row.iri),
-        "description": row.description
+        "description": row.description,
     }
+
 
 def get_classes(g, lang):
     classes = []
     for row in g.query(GET_CLASSES, initBindings={"lang": Literal(lang)}):
         classes.append(extract(g, row))
-        # add properties to class 
+        # add properties to class
         classes[-1]["properties"] = list(get_properties(g, row.iri, lang))
 
         # parent property that class GET_SUPERCLASS query from g and adds shortname
         classes[-1]["superclasses"] = []
         classes[-1]["subclasses"] = []
-        for parent in g.query(GET_SUPERCLASSES, initBindings={"lang": Literal(lang), "child": row.iri}):
-            
+        for parent in g.query(
+            GET_SUPERCLASSES, initBindings={"lang": Literal(lang), "child": row.iri}
+        ):
+
             classes[-1]["superclasses"].append(extract(g, parent))
             # print(list(get_properties(g, parent.iri)))
             classes[-1]["properties"].extend(list(get_properties(g, parent.iri, lang)))
-        for child in g.query(GET_SUBCLASSES, initBindings={"lang": Literal(lang), "parent": row.iri}):
+        for child in g.query(
+            GET_SUBCLASSES, initBindings={"lang": Literal(lang), "parent": row.iri}
+        ):
             classes[-1]["subclasses"].append(extract(g, child))
     return classes
 
+
 def get_properties(g, c, lang):
     properties = []
-    qres = g.query(GET_PROPERTIES, initBindings={"lang": Literal(lang), "targetClass": c})
+    qres = g.query(
+        GET_PROPERTIES, initBindings={"lang": Literal(lang), "targetClass": c}
+    )
     for row in qres:
         properties.append(extract(g, row))
         if row.get("datatype"):
             properties[-1]["datatype"] = {
-                "label": to_label(g,row.datatype),
+                "label": to_label(g, row.datatype),
                 "iri": row.datatype,
-                "shortname": to_shortname(g,row.datatype),
+                "shortname": to_shortname(g, row.datatype),
             }
             if bool(row.thesaurus):
                 properties[-1]["datatype"]["thesaurus"] = row.thesaurus
@@ -104,12 +120,12 @@ if __name__ == "__main__":
         help="SHACL OR RDFS files to construct Markdown documentation of.",
     )
     parser.add_argument(
-       "--language",
+        "--language",
         metavar="language",
         type=str,
         default="nl",
         required=False,
-        help="language of generated documentation, default is \"nl\"",
+        help='language of generated documentation, default is "nl"',
     )
     args = parser.parse_args()
     # print(args.accumulate(args.files))
